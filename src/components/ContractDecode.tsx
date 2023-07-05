79043d597a613363a953db20dcebcd722592bdf6
Colin Platt <colingplatt@gmail.com>
2023-07-05 15:42:38 +0200

    expand buttong

--- src/components/ContractDecode.tsx
+++ src/components/ContractDecode.tsx
@@ -101,10 +101,29 @@ export function ContractDecode({} : ContractDecodeProps) {
                                 {
                                 decodedMediaData ? (
                                     <Box sx={{ height: '95vh', overflow: 'hidden' }}>
+                                        <Button 
+                                            sx={{ 
+                                                marginLeft: '1rem',
+                                                color: '#000000',
+                                            }} 
+                                            onClick={() => {
+
+                                                const decoded = window.atob(decodedMediaData.split(',')[1]);
+                                                const win = window.open("", "_parent");
+
+                                                if (win) {
+                                                    win.document.write(decoded);
+                                                };
+                                            }}
+                                        >
+                                            Open original asset
+                                        </Button>
                                         <iframe 
                                             src={decodedMediaData}
                                             height="90%"
                                             width="90%"
+                                            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                                            sandbox="allow-same-origin allow-scripts allow-popups allow-modals allow-forms"
                                         />
                                     </Box>
                                 ) : null
